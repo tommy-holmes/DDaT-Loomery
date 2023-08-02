638b0467611aa5e46d8e33402ca879a835b479cf
Tom Holmes <thg.holmes96@gmail.com>
2023-08-02 14:55:40 +0100

    Only PoI

--- src/api/building/controllers/building.js
+++ src/api/building/controllers/building.js
@@ -4,6 +4,7 @@
  * building controller
  */
 
+// @ts-ignore
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::building.building');
